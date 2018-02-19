f4ee3d866c4b74b916b67838da9fc7e302cb8ee4
shashi <sekhar.shashi@gmail.com>
2018-02-19 20:35:34 +1100

    updating heroku port and version in package json

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,8 +1,5 @@
 const Joi = require('joi');
 
-const schema = {
-  address: Joi.object().required()
-}
 exports.filterJsonValidator = Joi.object({
-  payload: Joi.array().items(schema)
+  payload: Joi.array().items(Joi.object()).default([]).required()
 });
